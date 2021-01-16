8d6bc26b239ce685320c5577b25fd16aa3eea7a7
Sean Benson <sbenson@outlook.com>
2021-01-16 14:38:04 -0800

    Changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi Nicole
+          I was changed on the feature branch. Hello Nicole!
         </a>
       </header>
     </div>
